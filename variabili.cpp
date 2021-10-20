3142c85c1cc2fb94aa2bc53c10a7d4376a0e4126
Alessandro <alex_994@ymail.com>
2021-10-21 00:29:31 +0200

    Primo codice

---
+++ variabili.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,risultato;
+    cout<<"inserisci il primo numero intero\n";
+    cin>>a;
+    cout<<"inserisci il secondo numero intero\n";
+    cin>>b;
+    risultato = a+b;
+    cout<<"il riisultato è "<<risultato<<endl;
+    return 0;
+}
