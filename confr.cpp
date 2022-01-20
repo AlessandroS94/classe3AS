60bbf53e742461ab44df2ddbb8ddee2bf54ebda1
Alessandro Sallese <alessandrosallese@MacBook-Pro-di-Alessandro.local>
2022-01-20 10:42:38 +0100

    esercizio funzioni in classe

--- confr.cpp
+++ confr.cpp
@@ -2,6 +2,38 @@
 
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
+    int i = 0;
+    while (i < 1)
+    {
+        cout << "Inserisci il lato 1" << endl;
+        int l1;
+        cin >> l1;
+        cout << "Inserisci il lato 2" << endl;
+        int l2;
+        cin >> l2;
+        cout << "Inserisci il lato 3 " << endl;
+        int l3;
+        cin >> l3;
+        if (l1 == l2 && l2 == l3 && l1 == l3)
+        {
+            cout << "Equilatero" << endl;
+        }
+        else
+        {
+            if (l1 == l2 || l1 == l3 || l2 == l3)
+            {
+                cout << "Isoscele" << endl;
+            }
+            else
+            {
+                cout << "Scaleno" << endl;
+            }
+        }
+        cout << "Inserisci 1 per uscire 0 altrimenti"<<endl;
+        cin >>i;
+    }
+    return 0;
 }
---
+++ funzioni.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int max (int a, int b){
+    if(a>b){
+        return a;
+    }
+    else
+        return b;
+    
+}
+int main(){
+    int input1, input2,input3,input4;
+    cout<<"Inserisci input 1"<<endl;
+    cin>>input1;
+    cout<<"Inserisci input 2"<<endl;
+    cin>>input2;
+    cout<<"Inserisci input 3"<<endl;
+    cin>>input3;
+    cout<<"Inserisci input 4"<<endl;
+    cin>>input4;
+
+    cout << max(max(input1,input2),max(input3,input4));
+    return 0;
+}
+
